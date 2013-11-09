db75f21740b475ce3564c0129b5c395e9fc618d5
cheph <cheph.exe@gmail.com>
2013-11-09 17:42:37 +0900

    NewUnityProject

---
+++ New Unity Project/Assets/Game/Script/TextManage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextManage : MonoBehaviour {
+	
+	public TextMesh []Text;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void ChangeText(string mes){
+		int loopCounter;
+		for(loopCounter=0;loopCounter<Text.Length;loopCounter++){
+			Text[loopCounter].text=mes;
+		}
+	//	GameObject childObject = gameObject.transform.FindChild("Text000").gameObject;
+		
+	}
+}
---
+++ New Unity Project/Assets/Game/Script/shoot.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+	
+	public GameObject BlockPrefab;
+	public int ShootPow=10;
+		
+	Quaternion OVR_angle;
+	GameObject PlayerObj;
+	int a=0;
+	// Use this for initialization
+	void Start () {
+		PlayerObj=GameObject.Find("Player");
+		OVR_angle = Quaternion.identity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if(Input.GetKey(KeyCode.B)){
+		if (a%60==0){
+ 			Shoot("a");}
+		a++;
+			
+		//}
+	
+	}
+	void Shoot(string mes){
+
+		OVRDevice.GetPredictedOrientation(0, ref OVR_angle);
+		GameObject createBlock	= (GameObject)Instantiate(this.BlockPrefab, PlayerObj.transform.position, OVR_angle);
+		
+		createBlock.SendMessage("ChangeText",mes);
+		createBlock.rigidbody.velocity=OVR_angle*new Vector3(0,5,ShootPow);
+//		this.gameObject.rigidbody.velocity=new Vector3(ShootPow,0,0);
+		
+		
+	}
+}
